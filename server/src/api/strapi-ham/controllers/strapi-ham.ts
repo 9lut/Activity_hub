aade7b6f3e0da8ce7e81ef6df30d78efd5c77fd9
Sila Meksikharin <silastudyandwork@gmail.com>
2023-02-21 21:06:19 +0700

    check Ham

---
+++ server/src/api/strapi-ham/controllers/strapi-ham.ts
@@ -0,0 +1,7 @@
+/**
+ * strapi-ham controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::strapi-ham.strapi-ham');
---
+++ server/src/api/strapi-ham/routes/strapi-ham.ts
@@ -0,0 +1,7 @@
+/**
+ * strapi-ham router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::strapi-ham.strapi-ham');
