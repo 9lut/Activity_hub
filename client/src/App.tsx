b6f4f04b63726d9496b8f19c535811fad213a8ce
ลุตฟี ดอเล๊าะ <lutfee2017@gmail.com>
2023-02-23 01:24:59 +0700

    แก้ชื่อกับให้และเชื่อมกับ strapi

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './pages/login';
-import Profile from './pages/profile';
-import Sigin from './pages/signin';
+import Home from './pages/Home';
+import Register from './pages/register';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={< Login />} />
         <Route path="login" element={< Login />} />
-        <Route path="profile" element={< Profile />} />
+        <Route path="home" element={<Home />} />
       </Routes>
     </div>
   );
