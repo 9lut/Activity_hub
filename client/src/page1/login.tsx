9244495123c1fb7e373346b4d78be14e918cbc39
ลุตฟี ดอเล๊าะ <lutfee2017@gmail.com>
2023-02-26 02:16:01 +0700

    แก้ ล็อคอินให้เชื่อมกับ strapi และปุ่มล้คอิน google

--- client/src/page1/login.tsx
+++ client/src/page1/login.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import axios from "axios";
+import Swal from "sweetalert2";
 import './login.css';
 
 const Login: React.FC = (props) => {
@@ -8,80 +9,93 @@ const Login: React.FC = (props) => {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  
-    if (!email || !password) {
-      console.log('Please provide both email and password.');
-      return;
-    }
-  
-    try {
-      const response = await fetch('http://localhost:1337/api/auth/local', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          identifier: email,
-          password: password
-        })
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:1337/api/auth/local", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        identifier: email,
+        password,
+      }),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      localStorage.setItem("token", data.jwt);
+      Swal.fire({
+        icon: "success",
+        title: "คุณล็อคอินสำเร็จ",
+        text: "ยินดีต้อนรับเข้าสู่ ACTIVITY_HUB",
+      }).then(() => {
+        navigate("/home");
       });
-  
-      if (response.ok) {
-        const data = await response.json();
-        // save the token to local storage or global state
-        navigate('/dashboard');
-      } else {
-        console.log('Login failed.');
-      }
-    } catch (error) {
-      console.log(error);
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "คุณป้อนอีเมลหรือรหัสผ่านไม่ถูกต้อง",
+        text: "Please enter again!",
+      });
     }
   };
   
-
-  function handleEmailChange(event: ChangeEvent<HTMLInputElement>): void {
-    throw new Error("Function not implemented.");
-  }
-
   return (
-    <div className="container">
-    <div className="image">
-    <form className="form">
-      <p className="heading">
-        เข้าสู่ระบบ</p>
-      <p className="Username">
-        บัญชีผู้ใช้
-      </p>
+<div className="container">
+  <div className="image">
+    <form className="form" onSubmit={handleSubmit}>
+      <p className="heading">เข้าสู่ระบบ</p>
+      <p className="Username">บัญชีผู้ใช้</p>
       <div className="field">
-      <svg className="input-icon" xmlns="http://www.w3.org/2000/svg" 
-      width="16" height="16" 
-      fill="currentColor" 
-      viewBox="0 0 16 16">
-      </svg>
-        <input placeholder="Username" className="input-field"
-        onChange={(e) => setEmail(e.target.value)}
-        type="text"/>
+        <svg
+          className="input-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+        </svg>
+        <input
+          placeholder="Username"
+          className="input-field"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
-      <p className="Password">
-        รหัสผ่าน
-      </p>
+      <p className="Password">รหัสผ่าน</p>
       <div className="field">
-      <svg className="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-      </svg>
-        <input placeholder="Password" className="input-field" 
-        onChange={(e) => setPassword(e.target.value)}
-        type="password"/>
+        <svg
+          className="input-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+        </svg>
+        <input
+          placeholder="Password"
+          className="input-field"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
-      <p className="Tag">
-        ยังไม่มีบัญชีใช่ไหม?</p>
+      <span>คุณยังไม่มีบัญชีใช่ไหม? <a href="#" className="link signup-link">สมัครที่นี้</a></span>
       <div className="btn">
-      <button className="button1" onClick={() => navigate("/Home")}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ลงชื่อเข้าใช้&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+        <button className="button1" type="submit">ลงชื่อเข้าใช้</button>
+      </div>
+      <div className="media-options">
+        <a href="#" className="field google">
+          <img src="#" alt="" className="google-img" />
+          <span>เข้าสู่ระบบโดย Google</span>
+        </a>
       </div>
     </form>
   </div>
-  </div>
+</div>
   );
 };
 
