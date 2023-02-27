1064de3ab308f67f7b67d385d70c38e75fae9317
ilham53295 <ihsaniah112@gmail.com>
2023-02-27 23:09:41 +0700

    แก้ไข App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,11 +6,8 @@ import Login from './page1/login';
 import Home from './page1/Home';
 import Register from './page1/register';
 import AppBar from "./components/appbar";
-<<<<<<< HEAD
 import Profile from './page1/Profile'
-=======
 import Homepage from './page1/Homepage';
->>>>>>> 37aaa05 (เพิ่มหน้ารองก่อนเข้าหน้าหลัก)
 
 function App() {
   return (
@@ -20,11 +17,8 @@ function App() {
         <Route path="login" element={< Login />} />
         <Route path="Home" element={< Home />} />
         <Route path="register" element={<Register/>} />
-<<<<<<< HEAD
         <Route path="/Profile" element={<Profile />} />
-=======
         <Route path="homepage" element={<Homepage />}/>
->>>>>>> 37aaa05 (เพิ่มหน้ารองก่อนเข้าหน้าหลัก)
       </Routes>
     </div>
   );
