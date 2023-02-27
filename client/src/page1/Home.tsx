4b6e8ff2b162fa8dea81984d64f01b38d4bd8e4a
Maisoon Kentasa <6510110379@email.psu.ac.th>
2023-02-28 00:08:08 +0700

    Update Home and add background

--- client/src/page1/Home.tsx
+++ client/src/page1/Home.tsx
@@ -3,9 +3,11 @@ import { Grid, MenuItem, Select, SelectChangeEvent, TextField, Typography } from
 import { Box } from '@mui/system';
 import AppBar from '../components/appbar';
 import Result from '../models/Result';
+import backgroundImage from '../image/backgroundhome.jpg';
+import './Home.css';
 
 function Home() {
-  const [Home, setHome] = useState<Result[]>([])
+  const [home, setHome] = useState<Result[]>([])
   const [selectFilter, setSelectFilter] = useState('');
   const [searchFilter, setSearchFilter] = useState('');
 
@@ -24,20 +26,20 @@ function Home() {
     setSelectFilter(event.target.value);
   };
   
-
   useEffect(() => {
     fetchHome()
   }, [searchFilter])
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar></AppBar>
+    <Box className="home-container">
+      <AppBar />
       <Select
-        sx={{ m: 2, minWidth: 120 }}
+        className="filter-select"
         value={selectFilter}
         onChange={handleChangeSelectFilter}
         displayEmpty
       >
+      
         <MenuItem value=''>
           ค้นหาประเภทกิจกรรม
         </MenuItem>
@@ -45,17 +47,7 @@ function Home() {
         <MenuItem value={2}>กิจกรรมค่าย</MenuItem>
         <MenuItem value={3}>กิจกรรมวิ่ง</MenuItem>
       </Select>
-      <TextField sx={{ m: 2, minWidth: 120 }} label="ชื่อกิจกรรม" placeholder="ค้นหาชื่อกิจกรรม" variant="outlined" value={searchFilter} onChange={handleChangeSearchFilter} />
-      {Home.length
-        ?
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12, lg: 12, xl: 10 }}>
-        </Grid>
-        :
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 400 }}>
-          <Typography variant="body2" color="text.secondary">No Result Found</Typography>
-        </Box>
-      }
-
+      <TextField className="search-input" label="ชื่อกิจกรรม" placeholder="ค้นหาชื่อกิจกรรม" variant="outlined" value={searchFilter} onChange={handleChangeSearchFilter}/>
     </Box>
   );
 };
