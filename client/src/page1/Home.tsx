a2747cc40a63a55e524ccff4f7fa0d5b2194e08d
Maisoon Kentasa <6510110379@email.psu.ac.th>
2023-03-05 21:57:13 +0700

    fix Home

--- client/src/page1/Home.tsx
+++ client/src/page1/Home.tsx
@@ -5,7 +5,6 @@ import AppBar from '../components/appbar';
 import Result from '../models/Result';
 import backgroundImage from '../image/backgroundhome.png';
 import './Home.css';
-import { Container } from 'react-bootstrap';
 
 function Home() {
   const [home, setHome] = useState<Result[]>([])
@@ -32,40 +31,42 @@ function Home() {
   }, [searchFilter])
 
   return (
+  <div>
     <Box className="home-container">
       <AppBar />
-      <Box className="search-box">
-        <TextField
-          className="search-input"
-          label="ชื่อกิจกรรม"
-          placeholder="ค้นหาชื่อกิจกรรม"
-          variant="outlined"
-          value={searchFilter}
-          onChange={handleChangeSearchFilter}/>
-      
+        <Box className="search-box">
+          <TextField
+            className="search-input"
+            label="ชื่อกิจกรรม"
+            placeholder="ค้นหาชื่อกิจกรรม"
+            variant="outlined"
+            value={searchFilter}
+            onChange={handleChangeSearchFilter}/>
+        
 
 
-        <Select
-          className="filter-select"
-          value={selectFilter}
-          onChange={handleChangeSelectFilter}
-          displayEmpty
-        >
-          <MenuItem disabled value="">
-            ประเภทกิจกรรม
-          </MenuItem>
-            <MenuItem value={1}>
-              <p className="search-all">ทั้งหมด</p>
+          <Select
+            className="search-select"
+            value={selectFilter}
+            onChange={handleChangeSelectFilter}
+            displayEmpty
+          >
+            <MenuItem disabled value="">
+              ประเภทกิจกรรม
             </MenuItem>
-            <MenuItem value={2}>
-              <p className="search-run">กิจกรรมวิ่ง</p>
-            </MenuItem>
-            <MenuItem value={3}>
-              <p className="search-camp">กิจกรรมค่าย</p>
-            </MenuItem> 
-        </Select>
-      </Box>
+              <MenuItem value={1}>
+                <p className="search-all">ทั้งหมด</p>
+              </MenuItem>
+              <MenuItem value={2}>
+                <p className="search-run">กิจกรรมวิ่ง</p>
+              </MenuItem>
+              <MenuItem value={3}>
+                <p className="search-camp">กิจกรรมค่าย</p>
+              </MenuItem> 
+          </Select>
+        </Box>
     </Box>
+  </div>
   );
 };
 
