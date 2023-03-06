6db4b675b05cc81ec8205e7a450b05953e224a69
ลุตฟี ดอเล๊าะ <lutfee2017@gmail.com>
2023-03-07 02:17:44 +0700

    แก้ไขเพิ่มเวลากิจกรรมและเพิ่มข้อมูลกิจกรรม

--- server/src/api/ham/controllers/ham.ts
+++
@@ -1,7 +0,0 @@
-/**
- * ham controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::ham.ham');
--- server/src/api/ham/routes/ham.ts
+++
@@ -1,7 +0,0 @@
-/**
- * ham router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::ham.ham');
