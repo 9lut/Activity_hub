93c8b3aa4bd2a30b530ee81ae07e1231b09a5e82
ลุตฟี ดอเล๊าะ <lutfee2017@gmail.com>
2023-03-07 16:53:02 +0700

    แก้ detailAct.tsx

--- client/src/components/DetailAct.tsx
+++ client/src/components/DetailAct.tsx
@@ -4,22 +4,29 @@ import { Link } from "react-router-dom";
 function DetailAct() {
   
   return (
-    <div className="images" style={{ display: "flex", flexDirection: "column" }}>
-      <img
-        src='https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80'
-        alt=""
-        style={{ width: "100%", height: "100%" }}
-      />
+    <>
+    <header className="header">
+      <div className="container">
+      </div>
+    </header>
+
+    <section className="Detail">
       <div style={{ margin: "20px 0 0 20px", textAlign: "center" }}>
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginBottom: "20px" }}>
-        <p style={{ fontSize: "40px", fontWeight: "bold", margin: 0 }}>Take Photo Practice Camp</p>
+        <p style={{ fontSize: "30px", fontWeight: "bold", margin: 0 }}>
+          Take Photo Practice Camp
+          </p>
       </div>
-        <p style={{ fontSize:"24px", fontWeight: 300, margin: 0 }}>วันที่จัดกิจกรรม : 25-26 มกราคม 2022</p>
-        <p style={{ fontSize:"24px", fontWeight: 300, margin: 0 }}>สถานที่จัดกิจกรรม: สวนขวัญเมือง</p>
+        <p style={{ fontSize:"20px", fontWeight: 300, margin: 0 }}>
+          วันที่จัดกิจกรรม : 25-26 มกราคม 2022
+          </p>
+        <p style={{ fontSize:"20px", fontWeight: 300, margin: 0 }}>
+          สถานที่จัดกิจกรรม: สวนขวัญเมือง
+          </p>
         <br />
         <div style={{ backgroundColor: "#f2f2f2", display: "inline-block", borderRadius: "10px" }}>
-          <p style={{ fontSize: "30px", fontWeight: "bold", margin: "20px" }}>รายละเอียดของกิจกรรม</p>
-          <p style={{ fontSize: "24px", fontWeight: 300, margin: "20px" }}>
+          <p style={{ fontSize: "25px", fontWeight: "bold", margin: "20px" }}>รายละเอียดของกิจกรรม</p>
+          <p style={{ fontSize: "16px", fontWeight: 300, margin: "20px" }}>
             เป็นกิจกรรมเปิดให้ผู้ที่มีความสนใจในการถ่ายรูป
           </p>
         </div>
@@ -27,28 +34,40 @@ function DetailAct() {
         <br />
         <hr style={{ borderColor: "#f2f2f2" }} />
         <br />
+        
+        <div className="register">
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", marginBottom: "20px" }}>
           <div style={{ backgroundColor: "#ff8800", padding: "20px", borderRadius: "10px" }}>
-              <p style={{ fontSize: "30px", fontWeight: "bold", margin: 0 }}>การเปิดรับสมัคร</p>
+              <p style={{ fontSize: "28px", fontWeight: "bold", margin: 0 }}>
+                การเปิดรับสมัคร
+                </p>
           </div>
           <br />
           <div style={{ backgroundColor: "#f2f2f2", padding: "20px", borderRadius: "10px", textAlign: "center" }}>
-            <p style={{ fontSize:"24px", fontWeight: 300, margin: 0 }}>เริ่มเปิดรับสมัคร: 12 มกราคม 2022</p>
-            <p style={{ fontSize:"24px", fontWeight: 300, margin: 0 }}>ปิดรับสมัคร: 20 มกราคม 2022</p>
+            <p style={{ fontSize:"24px", fontWeight: 300, margin: 0 }}>
+              เริ่มเปิดรับสมัคร: 12 มกราคม 2022
+              </p>
+            <p style={{ fontSize:"24px", fontWeight: 300, margin: 0 }}>
+              ปิดรับสมัคร: 20 มกราคม 2022
+              </p>
             <br />
-            <p style={{ fontSize:"32px", fontWeight: "bold", margin: 0 }}>ปิดรับสมัครเมื่อจำนวนคนที่สมัครเต็ม</p>
+            <p style={{ fontSize:"25px", fontWeight: "bold", margin: 0 , color:"red"}}>
+              ปิดรับสมัครเมื่อจำนวนคนที่สมัครเต็ม
+              </p>
           </div>
           <br />
+
           <Link to="/from">
-            <Button variant="contained" style={{ fontSize: "24px", padding: "20px" }}>
+            <Button variant="contained" style={{ fontSize: "20px", padding: "10px" }}>
               สมัครเลย!!
             </Button>
           </Link>
         </div>
+        </div>
       </div>
-    </div>
+    </section>
+    </>
   );
 }
 
 export default DetailAct;
- 
