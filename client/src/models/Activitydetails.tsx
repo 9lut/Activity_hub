0c8f00117f0b2f42c2d28b8c9c89bb364450f394
ลุตฟี ดอเล๊าะ <lutfee2017@gmail.com>
2023-03-08 14:41:55 +0700

    แก้ Activitydetails ใน Models

--- client/src/models/Activitydetails.tsx
+++ client/src/models/Activitydetails.tsx
@@ -1,7 +1,37 @@
-export default interface Activitydetails{
-    id: number
-    picture: string
-    topic: string
-    date: string
-    location: string
+export default interface ActivityDetails{
+    id: number;
+    attributes: {
+        image: any;
+        event: React.ReactNode;
+        name: string;
+        location: string;
+        categories: {
+            data: [
+                {
+                    id: number;
+                    attributes: {
+                        name : string;
+                    };
+                }
+            ];
+        };
+        cover: {
+            data: {
+                id: number;
+                attributes: {
+                    name: string;
+                };
+            };
+        };
+        activity_date: {
+            data: {
+                attributes: {
+                    first_date: string;
+                    end_date: string;
+                    event: string;
+                };
+            };
+        };
+    };
   }
+  
